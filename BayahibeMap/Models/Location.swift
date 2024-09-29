a4d6e6029f0c4608b804df7912aa0b8292fdeefc
Alperen Sarışan <88689564+althebeast@users.noreply.github.com>
2024-09-28 22:05:19 -0400

    DataServices and the model of the that file is ready.

---
+++ BayahibeMap/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  BayahibeMap
+//
+//  Created by Alperen Sarışan on 28.09.2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
