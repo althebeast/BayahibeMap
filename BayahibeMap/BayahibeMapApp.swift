e0d138958a2f4371c5ea61f16f6b5f6fd1b686db
Alperen Sarışan <88689564+althebeast@users.noreply.github.com>
2024-09-28 21:43:06 -0400

    Initial Commit

---
+++ BayahibeMap/BayahibeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BayahibeMapApp.swift
+//  BayahibeMap
+//
+//  Created by Alperen Sarışan on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BayahibeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
